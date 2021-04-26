e4562d8d34abbcbbd0f27676fb0ad95e01b58809
kyle_rood <kyle.rood@nanovest.co>
2021-04-25 20:16:09 -0700

    added code for outputting to pickkle aqnd setting up qtable

--- kyle.py
+++ kyle.py
@@ -1,6 +1,8 @@
 from api_interaction import *
 import pandas as pd
 import numpy as np
+import pickle
+
 
 #10 worlds
 #Each is 40x40
@@ -8,9 +10,10 @@ import numpy as np
     #save all of these and output to pickle file
 
 #returns the move you should do and updates the pickle file of all the new states
-# def q_learning():
-    #initialize the q table (number of actions x number of states)
-        #all are 0
+def choose_move():
+    #qlearning
+    #load qtable from pkl file
+    q_table = pickle.load(open('q_table.pkl', 'rb'))
 
     #choose and perform an action --> could be random
         #action (a) in the state (s) is chosen based on the Q-Table
@@ -22,6 +25,15 @@ import numpy as np
     #Evaluate --> update the Q(s, a) function based on Bellman Equation
         #New Q value (s, a) = (Current Q values) + (Learning Rate)[(Reward for taking an action in a state) + (Discount rate)(Maximum expected future reward (given current state)) - (Current Q values)] 
 
+    #output initially to pkl file
+    #dump embeddings into output file
+    with open('q_table.pkl', 'wb') as p:
+        pickle.dump(q_table, p)
+        print('done with dump')
+
+    move = "N"
+    return move
+
 #initialize some things
 world_num = "0"
 x = "10"
@@ -31,20 +43,26 @@ for i in range(40):
     for j in range(40):
         labels.append(str(i) + "," + str(j))
 
-# q_table = [[0 for col in range(5)] for row in range(10)]
-
 cols = ["position", "left", "right", "up", "down"]
 q_table = pd.DataFrame(index = [0], columns = cols).fillna(0)
 
-for label in labels:
-    q_table["position"].append(label)
+for i in range(len(labels)):
+    q_table.loc[i] = [labels[i], 0, 0, 0, 0]
+
 
 print(q_table.head())
+
+#output initially to pkl file
+#dump embeddings into output file
+with open('q_table_initial.pkl', 'wb') as p:
+    pickle.dump(q_table, p)
+    print('done with dump')
 
 enter_world(world_num)
 
 while(1):
-    move = "N"
+    # move = "N"
+    move = choose_move()
     make_move(move, world_num)
     
     sleep(1)
